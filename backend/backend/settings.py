f8a180c02841a7435b47acefed0e6dd64158528b
Александр Катышев <katyshevav@yandex.ru>
2025-03-30 13:55:46 +0700

    sdfds

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.131', '172.26.192.1', '172.19.0.2', 'localhost']
+ALLOWED_HOSTS = ['192.168.1.131', '172.26.192.1', '172.19.0.2', 'localhost', 'biffustasks.zapto.org']
 
 
 # Application definition
